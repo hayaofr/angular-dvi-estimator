6fceb5ebfeb6b2f058bea98f29cd12f06e34fcc3
Samuel MARTIN <samuel.martin@ca-cp.fr>
2019-04-16 13:58:47 +0200

    Migration vers angular 7 et les dernières versions

--- src/app/specification/specification.component.ts
+++ src/app/specification/specification.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+import {FormDataService} from '../data/formData.service';
 
 @Component({
   selector: 'app-specification',
@@ -6,10 +8,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./specification.component.css']
 })
 export class SpecificationComponent implements OnInit {
+  title = 'La taille de la spécification';
+  specification = 0;
 
-  constructor() { }
+  constructor(private router: Router, private formDataService: FormDataService) { }
 
   ngOnInit() {
+    this.specification = this.formDataService.getTailleSpec();
   }
+
+  save(form: any): boolean {
+    if (!form.valid) {
+      return false;
+    }
+
+    this.formDataService.setTailleSpec(this.specification);
+    return true;
+  }
+
+  goToNext(form: any) {
+    if (this.save(form)) {
+      // Navigation vers la page suivante
+      this.router.navigate(['/result']);
+    }
+  }
+
+  onChange(newValue) {
+    this.specification = newValue;
+  }
+
 
 }
